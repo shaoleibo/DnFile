184283bc8c6d74d4e6f67228826db388eb2dc608
shaoleibo <shaoleibo@gmail.com>
2013-08-19 00:54:07 +0800

    Game项目

---
+++ Game/Mathf.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game
+{
+
+    public class Mathf
+    {
+        public const float PI = 3.14f;
+
+        public static float AngleToRadian( float angle )
+        {
+            return angle * PI / 180;
+        }
+
+
+    }
+}
